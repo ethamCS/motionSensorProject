c3aa9d082353c9d63c649883a2fc8c16dedf45bc
Greyson Sequino <gcsequino@gmail.com>
2021-04-26 19:33:16 -0600

    bleh

--- src/alarm_device.py
+++ src/alarm_device.py
@@ -1,4 +1,4 @@
-from gpiozero import MotionSensor
+from RPi.GPIO as GPIO
 from gpiozero import Buzzer
 from classes.network import Network
 import time
@@ -10,16 +10,19 @@ server = Network(12345)
 bz = Buzzer(17)
 
 # MotionSensor setup
-ms = MotionSensor(7)
+GPIO.setmode(GPIO.BCM)
+MS_PIN = 7
+
+GPIO.setup(MS_PIN, GPIO.IN)
 
 try:
     while True:
-        if ms.motion_detected:
+        if GPIO.input(MS_PIN):
             print("Motion detected!")
-        server.alert_server("Motion detected!")
-        bz.on()
-        time.sleep(3)
-        bz.off()
+            server.alert_server("Motion detected!")
+            bz.on()
+            time.sleep(3)
+            bz.off()
 except KeyboardInterrupt:
     print("Quitting")
     server.close_connection()
